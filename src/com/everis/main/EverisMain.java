d43bf47f22a025526323d417c20395a6818bc408
jhermiac <jhermiac@everis.com>
2021-03-25 12:45:37 +0100

    Version 2 - GIT

--- src/com/everis/main/EverisMain.java
+++ src/com/everis/main/EverisMain.java
@@ -14,10 +14,11 @@ public class EverisMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		System.out.println("Hola a tod@s");
-		
+
 		/* INTRODUCIR NUEVOS SALUDOS */
+		System.out.println("Buenas tardes a todos y a todas!");
 
 	}
 
